b2e9066916deabc853f66ed6014721c0f1b39cc8
Robert Gravina <rgravina@vmware.com>
2021-06-23 14:06:26 +0900

    Show a blank view controller in a navigation controller.

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#  XCUITest Example
+
+The purpose of this app to try out a few of the XCUITest methods. See this [Hacking With Swift article](https://www.hackingwithswift.com/articles/83/how-to-test-your-user-interface-using-xcode) for a tutorial on writing UI tests. 
+
+See the `XCUITestExampleUITests` folder for the tests.
--- XCUITestExample/ViewController.swift
+++ XCUITestExample/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  XCUITestExample
-//
-//  Created by Robert Gravina on 2021/06/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "XCUITest Example"
+        view.backgroundColor = .systemBackground
     }
-
-
 }
-
