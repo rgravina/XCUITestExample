5d606356b88f0bfb0116ef587e6fab226846c24a
Robert Gravina <rgravina@vmware.com>
2021-06-23 12:03:52 +0900

    Initial Commit

---
+++ XCUITestExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCUITestExample
+//
+//  Created by Robert Gravina on 2021/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
